0c6c53ffaa62ef4bf0103e86121bb8ac74856086
unknown <s00147514@mail.itsligo.ie>
2015-12-12 23:09:43 +0000

    fist merge attemt

---
+++ MyActivities/MantasNotifications.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Net.Http;
+using Windows.Storage;
+using System.Net.Http.Headers;
+using Windows.Networking.PushNotifications;
+using Windows.UI.Popups;
+using System.Threading.Tasks;
+using Windows.System.Profile;
+using Windows.Storage.Streams;
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace MyActivities
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MantasNotifications : Page
+    {
+        private static string BACKEND_ENDPOINT = "http://azurebackendapi20.azurewebsites.net";
+        public MantasNotifications()
+        {
+            this.InitializeComponent();
+
+            //LoginAndRegisterClick(sender, e);
+
+            Button b = mainGrid.Children.First() as Button;
+           /// b.Click = LoginAndRegisterClick();
+            
+
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        private async void PushClick(object sender, RoutedEventArgs e)
+        {
+           //set windows notification services as a connection option
+                await sendPush("wns", "wns", this.NotificationMessageTextBox.Text);
+            
+
+        }
+
+        private async Task sendPush(string pns, string userTag, string message)
+        {
+            var POST_URL = BACKEND_ENDPOINT + "/api/notifications?pns=" +
+                pns + "&to_tag=" + userTag;
+
+            using (var httpClient = new HttpClient())
+            {
+                var settings = ApplicationData.Current.LocalSettings.Values;
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", (string)settings["AuthenticationToken"]);
+
+                try
+                {
+                    await httpClient.PostAsync(POST_URL, new StringContent("\"" + message + "\"",
+                        System.Text.Encoding.UTF8, "application/json"));
+                }
+                catch (Exception ex)
+                {
+                    MessageDialog alert = new MessageDialog(ex.Message, "Failed to send " + pns + " message");
+                    await alert.ShowAsync();
+                }
+            }
+        }
+
+        private async void LoginAndRegisterClick(object sender, RoutedEventArgs e)
+        {
+            SetAuthenticationTokenInLocalStorage();
+
+            var channel = await PushNotificationChannelManager.CreatePushNotificationChannelForApplicationAsync();
+
+            // The "username:<user name>" tag gets automatically added by the message handler in the backend.
+            // The tag passed here can be whatever other tags you may want to use.
+            try
+            {
+                // The device handle used will be different depending on the device and PNS. 
+                // Windows devices use the channel uri as the PNS handle.
+                await new RegisterClient(BACKEND_ENDPOINT).RegisterAsync(channel.Uri, new string[] { "myTag" });
+
+                var dialog = new MessageDialog("Registered ");
+                dialog.Commands.Add(new UICommand("OK"));
+                await dialog.ShowAsync();
+                SendPushButton.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageDialog alert = new MessageDialog(ex.Message, "Failed to register with RegisterClient");
+                await alert.ShowAsync();
+            }
+        }
+
+        private void SetAuthenticationTokenInLocalStorage()
+        {
+            //get device unique id
+            HardwareToken myToken = HardwareIdentification.GetPackageSpecificToken(null);
+            IBuffer hardwareId = myToken.Id;
+            //set username and password for back end api authentication
+            string username = hardwareId.ToString();
+            string password = "";
+
+            var token = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(username + ":" + password));
+            ApplicationData.Current.LocalSettings.Values["AuthenticationToken"] = token;
+        }
+
+        private void BackToMainPage(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
+    }
+}
---
+++ MyActivities/Model/SQLActivity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lumia.Sense;
+using SQLite;
+
+namespace MyActivities.Model
+{
+    class SQLActivity
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public Activity Mode { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
